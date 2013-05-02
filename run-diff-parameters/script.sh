d3e97fd36b6627fd6adeb34dac057cdcbd5c8828
tcompa <tomtin@gmail.com>
2013-05-02 10:53:58 +0200

    1st commit, working

---
+++ run-diff-parameters/script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+INPUTFILE=input.dat
+SOURCE=program.c
+PROGRAM=program.x
+
+#compile the source code (in this case it's very simple)
+gcc -Wall $SOURCE -o $PROGRAM
+
+touch $INPUTFILE
+
+#cycle on all the parameters
+for ((alpha=1;alpha<=3;alpha++)); do
+ for ((beta=1;beta<=3;beta++)); do
+
+  echo "alpha=$alpha, beta=$beta: start"
+
+  #prepare the input file:
+  rm $INPUTFILE
+  echo "$alpha $beta" > $INPUTFILE
+  echo "out-a$alpha-b$beta.dat" >> $INPUTFILE
+  #run the program:
+  ./$PROGRAM
+
+  echo "alpha=$alpha, beta=$beta: end"
+  echo
+
+ done
+done
