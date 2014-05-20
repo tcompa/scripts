532a7c7537308687b2fcadabeb5d99b5d2317ce5
Tommaso Comparin <comparin@lps.ens.fr>
2014-05-20 13:57:01 +0200

    typo

--- qstat_running_jobs.sh
+++ qstat_running_jobs.sh
@@ -4,6 +4,6 @@ user_jobs=`qstat -r | grep $USER | wc -l`
 tot_jobs=`qstat -Bf|grep nodect|cut -d' ' -f 7`
 others_jobs=$(($tot_jobs - $user_jobs))
 
-echo "User's running jobs: $user_jobs ($USER)"
-echo "Total running jobs:  $tot_jobs"
-echo "Oters' running jobs: $others_jobs"
+echo "User's running jobs:  $user_jobs ($USER)"
+echo "Total running jobs:   $tot_jobs"
+echo "Others' running jobs: $others_jobs"
