8eab77fe2e16a6d7595d17bcf86d290675a0a289
Tommaso Comparin <comparin@lps.ens.fr>
2015-08-11 19:35:16 +0200

    scripts up:\n python/aps \n gnuplot/fit

--- python/aps_article.py
+++ python/aps_article.py
@@ -3,65 +3,67 @@
 '''
 program:       aps_article.py
 author:        tc
-last modified: 2015-08-08 -- 17:30 CEST
+last modified: 2015-08-10 -- 12:00 CEST
 notes:         opens articles from APS journals (see link.aps.org)
 '''
 
-import os
+import sys
+import subprocess
 
 
+def get_input():
+    options = '| PRL: \'l\' [default]'
+    options += '| PRA: \'a\' | .. | PRX: \'x\' | RMP: \'r\''
+    journal = raw_input('| Journal?\n' + options + '\n')
+    if not journal:
+        journal = 'l'
+    volume = raw_input('| Volume? ')
+    page = raw_input('| Page? ')
+    if not volume or not page:
+        sys.exit('ERROR: missing input. Exit.')
+    return journal, volume, page
+
+
+# General options
 browser = 'firefox'
 proxy = ''
 # proxy = '.proxyname.someuniversity.fr'
 
+
+# Journal names: key/long/short
+list_keys = ['a', 'b', 'c', 'd', 'e', 'x', 'l', 'r']
+longname = {i: 'PhysRev%s' % i.capitalize()
+            for i in list_keys if i not in ['l', 'r']}
+longname['r'] = 'RevModPhys'
+longname['l'] = 'PhysRevLett'
+shortname = {i: 'pr%s' % i for i in list_keys if not i == 'r'}
+shortname['r'] = 'rmp'
+
+
+# Get and check input
+journal, volume, page = get_input()
+if journal not in list_keys:
+    sys.exit('ERROR: wrong journal key (\'%s\'). Exit.' % journal)
+if not volume.isdigit():
+    sys.exit('ERROR: wrong volume (\'%s\'). Exit.' % volume)
+if not page.isdigit():
+    sys.exit('ERROR: wrong page number (\'%s\'). Exit.' % page)
+print '| Journal: %s' % longname[journal]
+print '| Volume:  %s' % volume
+print '| Page:    %s' % page
+
+
+# Open link
+link = 'http://journals.aps.org%s/' % proxy
+link += '%s/abstract/10.1103/' % shortname[journal]
+link += '%s.%s.%s' % (longname[journal], volume, page)
+print '| Now opening %s' % link
+subprocess.Popen([browser, link])
+
+
+# Print doi
+doi = '10.1103/%s.%s.%s' % (longname[journal], volume, page)
+doi_link = 'http://dx.doi.org/' + doi
+print '| doi: %s' % doi
+print '| doi_link: %s' % doi_link
 print
-print '*' * 80
-print 'Open article link from APS journals (PRL, PR{A,B,C,D,E,X}, RMP)'
-print
-
-list_pr = ['a', 'b', 'c', 'd', 'e', 'x', 'l']
-longnames = {i: 'PhysRev%s' % i.capitalize() for i in list_pr}
-longnames['rmp'] = 'RevModPhys'
-shortnames = {i: 'pr%s' % i for i in list_pr}
-shortnames['rmp'] = 'rmp'
-
-# input
-OpenHome = False
-journal = raw_input('Journal? [default: PRL, others: a, b, c, d, e, x, rmp] ')
-if not journal:
-    journal = 'l'
-volume = raw_input('Volume? ')
-page = raw_input('Page? ')
-if not volume or not page or journal not in list_pr + ['rmp']:
-    print 'Something went wrong, open '
-    OpenHome = True
-
-print 'Journal: %s' % longnames[journal]
-
-# check volume
-if volume.isdigit():
-    print 'Volume:  %s' % volume
-else:
-    print 'Wrong volume number -> journal homepage'
-    OpenHome = True
-
-# check page
-if page.isdigit():
-    print 'Page:    %s' % page
-else:
-    print 'Wrong page number -> journal homepage'
-    OpenHome = True
-
-if OpenHome:
-    link = 'http://%s.aps.org%s' % (shortnames[journal], proxy)
-else:
-    link = 'http://journals.aps.org%s/' % proxy
-    link += '%s/abstract/10.1103/' % shortnames[journal]
-    link += '%s.%s.%s' % (longnames[journal], volume, page)
-
-print
-print 'Trying to open %s' % link
-print '**********************************************************************'
-print
-
-os.system('%s %s &' % (browser, link))
