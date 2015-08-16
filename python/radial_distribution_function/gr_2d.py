0303a4fa34a9b1984ad394ebda3c2715118f6831
Tommaso Comparin <comparin@lps.ens.fr>
2015-08-16 15:49:41 +0200

    updated gr_2d and gr_3d | added tests

--- python/radial_distribution_function/gr_2d.py
+++ python/radial_distribution_function/gr_2d.py
@@ -4,7 +4,7 @@
 program:       gr_2d.py
 author:        tc
 created:       2015-10-16 -- 00:30 CEST
-last-modified: 2015-10-16 -- 00:15 CEST
+last-modified: 2015-10-16 -- 15:30 CEST
 description:   Computes g(r) for a set of configurations of N two-dimensional
                points uniformly distributed in a square.
 notes:         The computation of the full NxN distance matrix is not optimal,
@@ -14,6 +14,30 @@ notes:         The computation of the full NxN distance matrix is not optimal,
 
 import numpy
 import matplotlib.pyplot as plt
+
+
+def generate_one_conf(L, N, dim):
+    '''
+    Generates a configuration for N dim-dimensional points in a dim-dimensional
+    hypercube of edge L.
+    '''
+    return numpy.random.uniform(0.0, L, size=(N, dim))
+
+
+def compute_distances(x, L, N, dim):
+    '''
+    Given a configuration of N points in [0, L] hypercube, computes all pair
+    distances, with closest-image convention.
+    '''
+    assert x.shape[0] == N
+    # compute NxN distance matrix (with closest-image convention)
+    diff = x.reshape(N, 1, dim) - x
+    diff = numpy.remainder(diff, L)
+    diff[diff > L / 2.0] -= L
+    dist = (diff ** 2).sum(axis=2) ** 0.5
+    # store distances (without double counting)
+    dist = dist[numpy.triu_indices(N, k=1)].flatten()
+    return dist
 
 
 def compute_gr_2d(samples, N, nbins=100):
@@ -27,35 +51,33 @@ def compute_gr_2d(samples, N, nbins=100):
     return r, gr
 
 
-N = 300                    # number of points
-L = 1000.0 ** (1.0 / 2.0)  # edge of the cube
-rho = float(N) / L ** 2    # average density
+def plot_gr(r, gr, rho):
+    plt.figure()
+    plt.xlabel('$r$', fontsize=20)
+    plt.ylabel('$g(r)$', fontsize=20)
+    plt.plot(r, gr, 'k-')
+    plt.axhline(rho, c='r', ls='--', lw=2, label='$\\rho=%s$' % rho)
+    plt.legend()
+    plt.savefig('fig_gr_2d.pdf', bbox_inches='tight')
+    plt.close()
 
-indices_upper_triangle = numpy.triu_indices(N, k=1)
-n_pairs = N * (N - 1) / 2
 
-n_config = 1000
-samples = numpy.empty(n_config * n_pairs)
-for i_config in xrange(n_config):
-    # generate N points in the cube
-    x = numpy.random.uniform(0.0, L, size=(N, 2))
-    # compute NxN distance matrix (with closest-image convention)
-    diff = x.reshape(N, 1, 2) - x
-    diff = numpy.remainder(diff, L)
-    diff[diff > L / 2.0] -= L
-    dist = (diff ** 2).sum(axis=2) ** 0.5
-    # store distances (without double counting)
-    dist = dist[indices_upper_triangle].flatten()
-    samples[i_config * n_pairs:(i_config + 1) * n_pairs] = dist[:]
+if __name__ == '__main__':
 
-# compute g(r)
-r, gr = compute_gr_2d(samples, N, 200)
+    dim = 2       # number of dimensions
+    N = 300       # number of points
+    L = 1000.0    # linear size of the box
 
-# plot
-plt.xlabel('$r$', fontsize=20)
-plt.ylabel('$g(r)$', fontsize=20)
-plt.plot(r, gr, 'k-')
-plt.axhline(rho, c='r', ls='--', lw=2, label='$\\rho=%s$' % rho)
-plt.legend(loc='best', frameon=False)
-plt.savefig('fig_gr_2d.pdf', bbox_inches='tight')
-plt.show()
+    n_pairs = N * (N - 1) / 2
+
+    # generate samples
+    n_config = 100
+    samples = numpy.empty(n_config * n_pairs)
+    for i_config in xrange(n_config):
+        x = generate_one_conf(L, N, dim)
+        dist = compute_distances(x, L, N, dim)
+        samples[i_config * n_pairs:(i_config + 1) * n_pairs] = dist[:]
+
+    # compute and plot g(r)
+    r, gr = compute_gr_2d(samples, N, 200)
+    plot_gr(r, gr, float(N) / L ** dim)
---
+++ tests/test_gr_3d.py
@@ -0,0 +1,57 @@
+'''
+created: 2015-10-16 -- 15:30 CEST
+author: tc
+'''
+
+
+from nose.tools import assert_equal
+from nose.tools import assert_greater_equal
+from nose.tools import assert_less_equal
+
+import numpy
+import sys
+sys.path.append('../python/radial_distribution_function/')
+
+from gr_3d import generate_one_conf
+from gr_3d import compute_distances
+from gr_3d import compute_gr_3d
+from gr_3d import plot_gr
+
+
+class test_gr_3d():
+
+    def test_generate_one_conf(self):
+        N = 25
+        dim = 3
+        L = 100.0
+        x = generate_one_conf(L, N, dim)
+        assert_equal(x.shape, (N, dim))
+        assert_greater_equal(x.min(), 0.0)
+        assert_less_equal(x.max(), L)
+
+    def test_compute_distances(self):
+        N = 25
+        dim = 3
+        L = 100.0
+        x = generate_one_conf(L, N, dim)
+        dist = compute_distances(x, L, N, dim)
+        assert_equal(dist.shape[0], N * (N - 1) / 2)
+
+    def test_compute_gr_3d(self):
+        N = 25
+        dim = 3
+        L = 100.0
+        x = generate_one_conf(L, N, dim)
+        dist = compute_distances(x, L, N, dim)
+        r, gr = compute_gr_3d(dist, N, nbins=100)
+        assert_greater_equal(gr.min(), 0.0)
+        assert_less_equal(gr.max(), L * numpy.sqrt(dim))
+
+    def test_plot_gr(self):
+        N = 25
+        dim = 2
+        L = 100.0
+        x = generate_one_conf(L, N, dim)
+        dist = compute_distances(x, L, N, dim)
+        r, gr = compute_gr_3d(dist, N, nbins=100)
+        plot_gr(r, gr, N / float(L ** dim))
