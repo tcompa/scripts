22df452e2d9b6cc956af4bf2a5f25185be3901d0
Tommaso Comparin <comparin@lps.ens.fr>
2015-08-16 16:01:04 +0200

    removed plotting from test

--- tests/test_gr_2d.py
+++ tests/test_gr_2d.py
@@ -15,7 +15,6 @@ sys.path.append('../python/radial_distribution_function/')
 from gr_2d import generate_one_conf
 from gr_2d import compute_distances
 from gr_2d import compute_gr_2d
-from gr_2d import plot_gr
 
 
 class test_gr_2d():
@@ -46,12 +45,3 @@ class test_gr_2d():
         r, gr = compute_gr_2d(dist, N, nbins=100)
         assert_greater_equal(gr.min(), 0.0)
         assert_less_equal(gr.max(), L * numpy.sqrt(dim))
-
-    def test_plot_gr(self):
-        N = 10
-        dim = 2
-        L = 100.0
-        x = generate_one_conf(L, N, dim)
-        dist = compute_distances(x, L, N, dim)
-        r, gr = compute_gr_2d(dist, N, nbins=100)
-        plot_gr(r, gr, N / float(L ** dim))
--- tests/test_gr_3d.py
+++ tests/test_gr_3d.py
@@ -15,7 +15,6 @@ sys.path.append('../python/radial_distribution_function/')
 from gr_3d import generate_one_conf
 from gr_3d import compute_distances
 from gr_3d import compute_gr_3d
-from gr_3d import plot_gr
 
 
 class test_gr_3d():
@@ -46,12 +45,3 @@ class test_gr_3d():
         r, gr = compute_gr_3d(dist, N, nbins=100)
         assert_greater_equal(gr.min(), 0.0)
         assert_less_equal(gr.max(), L * numpy.sqrt(dim))
-
-    def test_plot_gr(self):
-        N = 10
-        dim = 2
-        L = 100.0
-        x = generate_one_conf(L, N, dim)
-        dist = compute_distances(x, L, N, dim)
-        r, gr = compute_gr_3d(dist, N, nbins=100)
-        plot_gr(r, gr, N / float(L ** dim))
