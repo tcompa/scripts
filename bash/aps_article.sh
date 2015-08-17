bb64165636f362b88f44da290b4beeaaa1659b98
Tommaso Comparin <comparin@lps.ens.fr>
2015-08-17 09:37:21 +0200

    minor aps_article.sh [ci-skip]

--- bash/aps_article.sh
+++ bash/aps_article.sh
@@ -5,32 +5,31 @@
 # last modified: 2015-08-08 -- 17:30 CEST
 # notes:         opens articles from APS journals (see link.aps.org)
 
-# Choose a browser
+
+# choose a browser
 BROWSER=firefox
 
-# If you want to use a proxy, uncomment the next line and set the correct address
+# to use a proxy, uncomment the next line and set the correct address
 #PROXY=.proxyname.someuniversity.fr
 
-
 echo
-echo '*********************************************************************'
-echo
+echo '**********************************************************************'
 echo 'Script to fetch articles from APS journals (PRL, PR{A,B,C,D,E,X}, RMP)'
 echo
 
-#Input
+# input
 read -p "Journal? [default: PRL, other options: a, b, c, d, e, x, rmp]  " JOURNAL
-read -p "Volume? " VOLUME
+read -p "Volume? " VOL
 read -p "Page? " PAGE
 HOMEPAGE=0
 
-# Check journal
-if [ "$JOURNAL" == 'a' ];   then JOURNAL=PRA; JOURNAL_long=PhysRevA; fi;
-if [ "$JOURNAL" == 'b' ];   then JOURNAL=PRB; JOURNAL_long=PhysRevB; fi;
-if [ "$JOURNAL" == 'c' ];   then JOURNAL=PRC; JOURNAL_long=PhysRevC; fi;
-if [ "$JOURNAL" == 'd' ];   then JOURNAL=PRD; JOURNAL_long=PhysRevD; fi;
-if [ "$JOURNAL" == 'e' ];   then JOURNAL=PRE; JOURNAL_long=PhysRevE; fi;
-if [ "$JOURNAL" == 'x' ];   then JOURNAL=PRX; JOURNAL_long=PhysRevX; fi;
+# check journal
+if [ "$JOURNAL" == 'a' ]; then JOURNAL=PRA; JOURNAL_long=PhysRevA; fi;
+if [ "$JOURNAL" == 'b' ]; then JOURNAL=PRB; JOURNAL_long=PhysRevB; fi;
+if [ "$JOURNAL" == 'c' ]; then JOURNAL=PRC; JOURNAL_long=PhysRevC; fi;
+if [ "$JOURNAL" == 'd' ]; then JOURNAL=PRD; JOURNAL_long=PhysRevD; fi;
+if [ "$JOURNAL" == 'e' ]; then JOURNAL=PRE; JOURNAL_long=PhysRevE; fi;
+if [ "$JOURNAL" == 'x' ]; then JOURNAL=PRX; JOURNAL_long=PhysRevX; fi;
 if [ "$JOURNAL" == 'rmp' ]; then JOURNAL=RMP; JOURNAL_long=RevModPhys; fi;
 
 JOURNAL_lo=`echo $JOURNAL | tr [:upper:] [:lower:]`
@@ -43,8 +42,8 @@ else
 fi
 
 # Check volume
-if [[ $VOLUME =~ ^[\-0-9]+$ ]] && (( VOLUME > 0)); then
- echo '  volume set to '$VOLUME
+if [[ $VOL =~ ^[\-0-9]+$ ]] && (( VOL > 0)); then
+ echo '  volume set to '$VOL
 else
  HOMEPAGE=1
  echo '  wrong volume number -> journal homepage'
@@ -52,15 +51,14 @@ fi
 echo '  page set to '$PAGE
 
 if [ "$HOMEPAGE" == '0' ]; then
- LINK=http://journals.aps.org$PROXY/$JOURNAL_lo/abstract/10.1103/$JOURNAL_long.$VOLUME.$PAGE
+ LINK=http://journals.aps.org$PROXY/$JOURNAL_lo/abstract/10.1103/$JOURNAL_long.$VOL.$PAGE
 else
  LINK=http://$JOURNAL_lo.aps.org$PROXY;
 fi
 
 echo
 echo 'Trying to open the following link: '$LINK
-echo
-echo '*********************************************************************'
+echo '**********************************************************************'
 echo
 
 $BROWSER $LINK &
