9a693137b3fe2d34db567091bf3a3b5b146d36f0
tcompa <comparin@lps.ens.fr>
2015-11-06 08:28:00 +0100

    minor: added I option to pip update

--- bash/pip_update_all.sh
+++ bash/pip_update_all.sh
@@ -3,4 +3,4 @@
 # update all pip packages
 # source: http://stackoverflow.com/a/3452888
 
-pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo -H pip install -U
+pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo -H pip install -UI
