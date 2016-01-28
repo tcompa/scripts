7e68d99b51fb8f7a0b89a63b8f28d10db630cb0b
tcompa <comparin@lps.ens.fr>
2016-01-28 16:11:16 +0100

    python/versions.py

---
+++ python/versions.py
@@ -0,0 +1,20 @@
+import sys
+import scipy
+import numpy
+import multiprocessing
+
+print '-' * 80
+print
+print 'sys.version:'
+print sys.version
+print
+print 'scipy.version:'
+print scipy.__version__
+print
+print 'numpy.version:'
+print numpy.__version__
+print
+print 'multiprocessing.version:'
+print multiprocessing.__version__
+print
+print '-' * 80
