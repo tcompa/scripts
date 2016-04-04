49882e2aa081676ac70829c6e077c433b174f529
Tommaso Comparin <comparin@lps.ens.fr>
2016-04-04 14:31:57 +0200

    histo_2d.py -- minor

--- matplotlib/histo_2d.py
+++ matplotlib/histo_2d.py
@@ -1,13 +1,28 @@
+'''
+program: histo_2d.py
+author: tc
+created: 2016-04-04 -- 14:30 CEST
+
+Notes:
+I use histogram2d+matshow instead of hexbin, since I find it easier for
+having a different resolution on the two axis.
+'''
+
 import numpy
 import matplotlib.pyplot as plt
+
 
 x = numpy.random.normal(0.0, 1.0, 10000)
 y = numpy.random.normal(0.0, 3.0, 10000)
 
-H, xe, ye = numpy.histogram2d(x, y, bins=(10, 30))
+H, x_edges, y_edges = numpy.histogram2d(x, y, bins=(10, 30))
+extent = [x_edges[0], x_edges[-1], y_edges[0], y_edges[-1]]
 
 fig, ax = plt.subplots(1, 1)
-ax.matshow(H, origin='lower', extent=[xe[0], xe[-1], ye[0], ye[-1]])
-ax.xaxis.set_ticks_position('bottom')
-ax.set_aspect(0.3)
+ax.matshow(H,                           # the matrix to plot
+           origin='lower',              # sets the origin on the bottom
+           extent=extent                # (left, right, bottom, up) limits
+           )
+ax.xaxis.set_ticks_position('bottom')   # moves the x-axis ticks below the plot
+ax.set_aspect(0.3)                      # change the aspect ratio
 plt.show()
